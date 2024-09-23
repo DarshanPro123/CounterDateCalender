10e5c1e9feb6e73fc625fd56dc3600dd2fc5288a
Darshanpro123 <darshanpro187@gmail.com>
2024-09-23 11:17:48 +0530

    Add Steps for counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,53 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const [steps, setSteps] = useState(0);
   // const date
   const date = new Date();
   date.setDate(date.getDate() + count);
   console.log(date);
   const decreseCount = () => {
     {
-      count > -20 ? setCount((c) => c - 1) : count;
+      count > -20 ? setCount((c) => c - steps) : count;
     }
   };
   const increseCount = () => {
     {
-      count <= 20 ? setCount((c) => c + 1) : count;
+      count < 20 ? setCount((c) => c + steps) : count;
+    }
+  };
+
+  const decreseSteps = () => {
+    {
+      steps > 0 ? setSteps((stp) => stp - 1) : steps;
+    }
+  };
+  const increseSteps = () => {
+    {
+      steps < 20 ? setSteps((stp) => stp + 1) : alert("No more , warning!! 🥵");
     }
   };
 
   return (
     <>
       <div>
+        <div className="step-box">
+          <h3>Steps:</h3>
+          <div className="btns">
+            {" "}
+            <button onClick={decreseSteps}>-</button>
+            <h2>{steps}</h2>
+            <button onClick={increseSteps}>+</button>
+          </div>
+        </div>
         <div className="counter-box">
-          <button onClick={decreseCount}> - </button>
-          <h2>{count}</h2>
-          <button onClick={increseCount}> + </button>
+          <h3>Counter</h3>
+          <div className="btns">
+            <button onClick={decreseCount}> - </button>
+            <h2>{count}</h2>
+            <button onClick={increseCount}> + </button>
+          </div>
         </div>
         <h1>
           {" "}
